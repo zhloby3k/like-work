590b4ef2712cc6dc65aefa6bd80b9d75717ccacf
zhloby3k <zhloby3k@gmail.com>
2016-01-11 13:32:08 +0500

    SyncAdapter

---
+++ app/src/main/java/com/example/gbyakov/likework/sync/LikeWorkSyncAdapter.java
@@ -0,0 +1,46 @@
+package com.example.gbyakov.likework.sync;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.SyncResult;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.gbyakov.likework.R;
+
+public class LikeWorkSyncAdapter extends AbstractThreadedSyncAdapter {
+
+    public final String LOG_TAG = LikeWorkSyncAdapter.class.getSimpleName();
+
+    public LikeWorkSyncAdapter(Context context, boolean autoInitialize) {
+        super(context, autoInitialize);
+    }
+
+    @Override
+    public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
+        Log.d(LOG_TAG, "Starting sync");
+    }
+
+    public static void syncImmediately(Context context) {
+        Bundle bundle = new Bundle();
+        bundle.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+        bundle.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
+
+        Account account = getSyncAccount(context);
+        if (account != null) {
+            ContentResolver.requestSync(account,
+                    context.getString(R.string.content_authority), bundle);
+        }
+    }
+
+    public static Account getSyncAccount(Context context) {
+        AccountManager am = AccountManager.get(context);
+        Account[] accounts = am.getAccountsByType(context.getString(R.string.sync_account_type));
+        return (accounts.length > 0) ? accounts[0] : null;
+    }
+
+}
